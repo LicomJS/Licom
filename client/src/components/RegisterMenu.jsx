1cfb627158f6784dc3a29e10a05606aaffc2d3ec
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2022-04-14 01:27:32 +0200

    text

--- client/src/components/RegisterMenu.jsx
+++ client/src/components/RegisterMenu.jsx
@@ -187,7 +187,7 @@ const RegisterMenu = ({ setAuth }) => {
             <button
               className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
               onClick={() => {
-                let cpk = prompt("What's your private key?");
+                let cpk = prompt("Confirm your private key");
                 if (cpk === key.privateKey) {
                   registerApi();
                 } else {
