063283c77d01bfcb2ec52025b93022438526cbcc
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2022-04-19 10:41:30 +0200

    cors

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const {
 } = require("./functions");
 
 const cors = corsMiddleware({
-  origins: ["http://localhost:3000", "http://web.myapp.com"],
+  origins: ["*"],
 });
 
 const srv = restify.createServer({
