d6b95b3e97754b73e829e5dd9520486004c9f281
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2022-04-29 06:16:45 +0200

    resizeable side-panel

--- client/public_firefox/side-panel.js
+++ client/public_firefox/side-panel.js
@@ -6,26 +6,65 @@ browser.runtime.onMessage.addListener((msg) => {
   }
 });
 
+var resizer = document.createElement("div");
+resizer.style.position = "fixed";
+resizer.style.top = "0px";
+resizer.style.right = "0px";
+resizer.style.zIndex = "9000000000000000000";
+resizer.style.borderLeft = "0px";
+resizer.style.background = "transparent";
+resizer.style.height = "100%";
+resizer.style.width = "0px";
+resizer.style.cursor = "ew-resize";
+resizer.style.transition = "left 0.2s, width 0.2s";
+
 var iframe = document.createElement("iframe");
-iframe.style.background = "white";
 iframe.style.height = "100%";
-iframe.style.width = "0px";
-iframe.style.position = "fixed";
-iframe.style.top = "0px";
-iframe.style.right = "0px";
-iframe.style.zIndex = "9000000000000000000";
+iframe.style.width = "100%";
 iframe.style.border = "0px";
-iframe.style.borderLeft = "1px solid #ccc";
-// iframe.style.transition = "all 0.5s linear";
-iframe.style.transition = "left 0.2s, width 0.2s";
 
 function toggle(url) {
-  if (iframe.style.width == "0px") {
-    iframe.style.width = "400px";
+  if (resizer.style.width == "0px") {
+    resizer.style.width = "400px";
+    resizer.style.borderLeft = "5px solid #666";
     iframe.src = browser.runtime.getURL("index.html?url=" + url);
   } else {
-    iframe.style.width = "0px";
+    resizer.style.width = "0px";
+    resizer.style.borderLeft = "0px";
   }
 }
 
-document.body.appendChild(iframe);
+resizer.addEventListener("pointerdown", initResize);
+
+let original_mouse_x;
+let original_width;
+
+function initResize(e) {
+  e.preventDefault();
+  resizer.style.transition = "";
+  iframe.style.pointerEvents = "none";
+
+  original_mouse_x = e.pageX;
+  original_width = parseFloat(resizer.style.width.replace("px", ""));
+  window.addEventListener("pointermove", Resize);
+  window.addEventListener("pointerup", stopResize);
+}
+
+function Resize(e) {
+  let width = original_width + (original_mouse_x - e.pageX);
+
+  if (e.pageX > 20 && width > 250) {
+    resizer.style.width = width + `px`;
+  }
+}
+
+function stopResize() {
+  iframe.style.pointerEvents = "all";
+  resizer.style.transition = "left 0.2s, width 0.2s";
+
+  window.removeEventListener("pointermove", Resize);
+  window.removeEventListener("pointerup", stopResize);
+}
+
+resizer.appendChild(iframe);
+document.body.appendChild(resizer);
